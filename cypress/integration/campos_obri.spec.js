34ff717071981b1e5dc97f83076b886a2b805f4f
NandoHessel <nandohessel@hotmail.com>
2022-03-06 20:12:03 -0300

    Correção nos campos obrigatórios

--- cypress/integration/campos_obri.spec.js
+++ cypress/integration/campos_obri.spec.js
@@ -30,20 +30,3 @@ context('required fields', function() {
         });
     })
 });
-
-it.skip('required field', function() {
-
-    var signup = new SignupPage()
-
-    signup.go()
-    signup.submit()
-    signup.alertMessageShouldBe('É necessário informar o nome')
-    signup.alertMessageShouldBe('É necessário informar o CPF')
-    signup.alertMessageShouldBe('É necessário informar o email')
-    signup.alertMessageShouldBe('É necessário informar o CEP')
-    signup.alertMessageShouldBe('É necessário informar o número do endereço')
-    signup.alertMessageShouldBe('Selecione o método de entrega')
-    signup.alertMessageShouldBe('Adicione uma foto da sua CNH')
-
-    
-}); 
