77b54fa21c6805018f6c6cc133878c3c94df6ad6
NandoHessel <nandohessel@hotmail.com>
2022-03-06 19:49:30 -0300

    ajustando para os padrões do Cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,7 +4,7 @@ describe('home page', () => {
     //caso de teste
     it('App deve estar online', () => {
         cy.viewport(1920,1080) //muda a resolução da tela
-        cy.visit('https://buger-eats.vercel.app/') //acessar a URL alvo do teste
+        cy.visit('/') //acessar a URL alvo do teste
         //valida se existe o texto dentro do elemento
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats') 
         
